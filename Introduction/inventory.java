0bb14e562b1b568975a6390e61bab42121699a6a
Vsk Code <vynycyusvieira@gmail.com>
2021-02-18 20:23:53 -0300

    Inserting New Exercises

---
+++ Introduction/inventory.java
@@ -0,0 +1,9 @@
+package Introduction;
+
+public class inventory {
+    public static void main(String[] args) {
+        int Minimum = 5;
+        int Maximum = 2;
+        System.out.println("O estoque médio é "+Minimum+Maximum/2);
+    }
+}
