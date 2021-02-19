9209092ee51261afb36c369799786fae364efa7e
Vsk Code <vynycyusvieira@gmail.com>
2021-02-19 20:04:24 -0300

    Update 0.2

---
+++ Estoque/src/inventory.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class inventory {
+    public static void main(String[] args) {
+        float Minimum = Float.parseFloat(JOptionPane.showInputDialog("Entre com o número minimo de peças:"));
+        float Maximum = Float.parseFloat(JOptionPane.showInputDialog("Entre com o número máximo de peças:"));
+        JOptionPane.showMessageDialog(null, "O estoque médio de peças é: "+(Minimum+Maximum)/2,"Estoque Médio",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
